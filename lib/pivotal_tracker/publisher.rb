61d2164fbf1e392c841ada154fda2531adecfae6
Mikhail Grachev <work@mgrachev.com>
2015-12-28 15:31:01 +0300

    Bugfix

--- lib/pivotal_tracker/publisher.rb
+++ lib/pivotal_tracker/publisher.rb
@@ -1,30 +1,28 @@
 module PivotalTracker
   class Publisher < Base
-    class << self
-      def run
-        redis.psubscribe "#{NAME}/activity/*" do |on|
-          on.psubscribe do |channel, _|
-            logger.info "Publisher -- Subscribed to ##{channel}"
-          end
+    def self.run
+      redis.psubscribe "#{NAME}/activity/*" do |on|
+        on.psubscribe do |channel, _|
+          logger.info "Publisher -- Subscribed to ##{channel}"
+        end
 
-          on.pmessage do |_, channel, message|
-            logger.info "Publisher -- Message received \"#{message}\" from the channel ##{channel}"
-            project_key = channel.split('/')[2]
-            redis_key = "#{NAME}/chat_id/#{project_key}"
+        on.pmessage do |_, channel, message|
+          logger.info "Publisher -- Message received \"#{message}\" from the channel ##{channel}"
+          project_key = channel.split('/')[2]
+          redis_key = "#{NAME}/chat_id/#{project_key}"
 
-            if redis_instance.exists(redis_key)
-              chat_id = redis_instance.get(redis_key)
-              telegram_bot.api.sendMessage(chat_id: chat_id, text: message, disable_web_page_preview: true)
-            end
+          if redis_instance.exists(redis_key)
+            chat_id = redis_instance.get(redis_key)
+            telegram_bot.api.sendMessage(chat_id: chat_id, text: message, disable_web_page_preview: true)
           end
         end
-      rescue => error
-        logger.fatal("Publisher -- Exception : #{error.message}\n#{error.backtrace.join("\n")}")
       end
+    rescue => error
+      logger.fatal("Publisher -- Exception : #{error.message}\n#{error.backtrace.join("\n")}")
+    end
 
-      def redis_instance
-        @redis_instance ||= Redis.new
-      end
+    def self.redis_instance
+      @redis_instance ||= Redis.new
     end
   end
 end
